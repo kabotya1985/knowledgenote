08c3cdb8f7de967ae8af380163c5d97be619f26f
tosi1985 <tosi1985@gmail.com>
2019-11-03 17:09:42 +0900

    110201
    Signed-off-by: tosi1985 <tosi1985@gmail.com>

--- app/app/Http/Controllers/Auth/LoginController.php
+++ app/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+    use AuthenticatesUsers;
 
 class LoginController extends Controller
 {
@@ -18,7 +19,7 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+
 
     /**
      * Where to redirect users after login.
@@ -36,4 +37,16 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function handle($request, Closure $next, $guard = null)
+    {
+      if (Auth::guard($guard)->check()) {
+        return redirect()->intended('/');
+      }
+      return $next($request);
+    }
+    protected function redirectTo()
+      {
+        return URL::previous();
+      }
 }
--- app/resources/views/Signin/form.blade.php
+++ app/resources/views/Signin/form.blade.php
@@ -1,202 +1,75 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Login Page HTML Template">
-    <title>Login Page HTML Template</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/fonts/fontawesome/css/all.css">
-    <link rel="stylesheet" href="/assets/flag-icon/flag-icon.min.css">
-    <link rel="stylesheet" href="/assets/css/theme-style.css">
-    <link rel="stylesheet" type="/text/css" href="/assets/select2/select2.min.css">
-</head>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
 
-<body>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
 
-      <div class="preloader">
-        <div class="lds-ripple">
-            <div class="lds-pos"></div>
-            <div class="lds-pos"></div>
-        </div>
-    </div>
-    <div class="main-wrapper">
-        <!-- header start -->
-        <div class="header-classic">
-            <!-- navigation start -->
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                        <nav class="navbar navbar-expand-lg navbar-classic">
-                            <a class="navbar-brand" href="/index"> <img src="/assets/images/logo.png" alt=""></a>
-                            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar-classic" aria-controls="navbar-classic" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="icon-bar top-bar mt-0"></span>
-                                <span class="icon-bar middle-bar"></span>
-                                <span class="icon-bar bottom-bar"></span>
-                            </button>
-                            
-                            <div class="collapse navbar-collapse" id="navbar-classic">
-                                <ul class="navbar-nav ml-auto mt-2 mt-lg-0 mr-3">
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                                    <li>
-                                    <a class="nav-link" href="/index">Homepage</a>       
-                                    </li>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                    <li>
-                                    <a class="nav-link" href="/framework/form">framework作成</a>       
-                                    </li>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
-                                    <li>
-                                    <a class="nav-link" href="/member">member</a>       
-                                    </li>
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                                </ul>
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                                
-                                    <a class="nav-link" href="/signin/form">
-                                        signin
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
                                     </a>
-                                </div>
-
+                                @endif
                             </div>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-            <!-- navigation close -->
-        </div>
-        <!-- header close -->
-        <!-- pagheader start -->
+                        </div>
 
-        <!-- pagheader close -->
-        <!-- breadcrumb start -->
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12 col-12">
-                    <div class="custom-breadcrumb">
-                        <nav aria-label="breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                                <li class="breadcrumb-item active">signin</li>
-                            </ol>
-                        </nav>
-                    </div>
+                        
+                    </form>
                 </div>
             </div>
         </div>
-        <!-- breadcrumb close -->
-        <div class="space-lg space-md space-xs">
-            <div class="container">
-                <div class="row">
-                    <div class="offset-xl-3 col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12">
-                        <!-- login form start -->
-
-                        <div class="login-form">
-                            <div class="login-form-body">
-                                <h4 class="login-form-title">signin</h4>
-
-                                <form>
-                                    <div class="form-group">
-                                        <label for="email">Email</label>
-                                        <input type="email" class="form-control" id="email" aria-describedby="email" placeholder="example@gmail.com" required="">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label id="password">Password</label>
-                                        <input id="password-field" type="password" class="form-control" name="password" value="" placeholder="*************">
-                                        <span toggle="#password-field" class="fa fa-fw fa-eye password-hide-icon showhidepassword"></span>
-                                        <small>Must be 8-20 characters long.</small>
-                                    </div>
-
-                                    <button type="submit" class="btn btn-primary btn-block">signin</button>
-                                </form>
-
-                                <div class="login-remember-password-block">
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="rememberme">
-                                        <label class="custom-control-label" for="rememberme">Remember Me</label>
-                                    </div>
-                                    <div class="forgot-password-link">
-                                        <a href="forget-password-page.html"> Forgot Password?</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="login-form-footer">
-                                <p class="login-form-footer-text">Don’t have a Spacely account? <a href="register-page.html">Create Account</a> and we’ll set you up so you can list your space.</p>
-                            </div>
-                        </div>
-                        <!-- signin form close -->
-                    </div>
-                </div>
-            </div>
-        </div>
-             <!-- footer section start -->
-         <div class="footer-dark">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-12">
-                          <!-- footer widget  -->
-                        <div class="footer-widget">
-                            <div class="brand-logo"><img src="/text/images/primary-white-logo.png" alt=""></div>
-                            <p class="footer-widget-text">詳細</p>
-                            <div class="footer-location">
-                                <p class="phone-numbers">000-0000-0000</p>
-                                <p class="mail">tosi1985@gmail.com</p>
-                            </div>
-                            <div class="social-media">
-                                <a href="#"><i class="fab fa-facebook-square"></i></a>
-                                <a href="#"><i class="fab fa-twitter-square"></i></a>
-                                <a href="#"><i class="fab fa-linkedin"></i></a>
-                                <a href="#"><i class="fab fa-pinterest-square"></i></a>
-                                <a href="#"><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                      <!-- footer widget  -->
-                   
-                      <!-- footer widget  -->
-                    
-                      <!-- footer widget  -->
-                    
-                          <!-- footer widget  -->
-                    </div>
-                </div>
-            </div>
-              <!-- tiny footer  -->
-            <div class="tiny-footer">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                            <p class="tiny-footer-text">Copyright © 2020 Spacely Companies Inc. All rights reserved</p>
-                        </div>
-                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                            <div class="tiny-footer-links">
-                                <ul>
-                                    <li><a href="#">Privacy</a></li>
-                                    <li><a href="#">Terms</a></li>
-                                    <li><a href="#">Cookies</a></li>
-                                    <li><a href="#">FAQ</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-              <!-- footer section close  -->
-        </div>
-         <!-- footer section close -->
     </div>
-    <!-- ============================================================== -->
-    <!-- end main wrapper -->
-    <!-- ============================================================== -->
-    <script src="/assets/js/jquery-3.3.1.min.js"></script>
-    <script src="/assets/js/bootstrap.bundle.js"></script>
-    <script src="/assets/js/main-js.js"></script>
-    <script src="/assets/select2/select2.full.min.js"></script>
-    <script src="/assets/select2/select2.min.js"></script>
-</body>
-
-</html>
+</div>
+@endsection
